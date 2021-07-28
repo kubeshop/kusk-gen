89d2abcac42126d39d1214cf3b8ab68d2cc2a1cd
Kyle Hodgetts <kyle@kubeshop.io>
2021-07-28 17:37:55 +0300

    Add support for specifying kusk extension options at the path and http method level. (#50)
    The idea behind this is to create an options chain where the lowest
    level options take precedence over the higher level ones. E.g. an option
    set at the path level will take precedence over the same option set at
    the global level for that path. Likewise for the HTTP method level: If a
    setting is present at the HTTP method level it overrides the same
    setting set at its path level for that HTTP method.
    
    If a setting isn't provided at the HTTP method or path level, then the
    options "chain" should be traversed until an option is found, ideally at
    the global level, and if not, then a sensible default value should be
    chosen, usually disabling whatever feature the option relates to.

--- options/options.go
+++ options/options.go
@@ -5,6 +5,8 @@ import (
 )
 
 type Options struct {
+	Disabled bool `yaml:"disabled,omitempty" json:"disabled,omitempty"`
+
 	// Namespace for the generated resource. Default value is "default".
 	Namespace string `yaml:"namespace,omitempty" json:"namespace,omitempty"`
 
@@ -22,6 +24,11 @@ type Options struct {
 
 	// NGINXIngress is a set of custom nginx-ingress options.
 	NGINXIngress NGINXIngressOptions `yaml:"nginx_ingress,omitempty" json:"nginx_ingress,omitempty"`
+
+	// TODO(kyle) add rate limiting and retries
+
+	PathOperations       map[string]Options
+	HTTPMethodOperations map[string]Options
 }
 
 func (o *Options) fillDefaults() {
@@ -39,6 +46,14 @@ func (o *Options) fillDefaults() {
 
 	if o.Service.Port == 0 {
 		o.Service.Port = 80
+	}
+
+	if o.PathOperations == nil {
+		o.PathOperations = map[string]Options{}
+	}
+
+	if o.HTTPMethodOperations == nil {
+		o.HTTPMethodOperations = map[string]Options{}
 	}
 }
 
--- spec/extension.go
+++ spec/extension.go
@@ -10,16 +10,62 @@ import (
 	"github.com/kubeshop/kusk/options"
 )
 
+const kuskExtensionKey = "x-kusk"
+
 // GetOptions would retrieve and parse x-kusk top-level OpenAPI extension
 // that contains Kusk options. If there's no extension found, an empty object will be returned.
 func GetOptions(spec *openapi3.T) (*options.Options, error) {
 	var res options.Options
 
-	if extension, ok := spec.Extensions["x-kusk"]; ok {
+	if extension, ok := spec.Extensions[kuskExtensionKey]; ok {
 		if kuskExtension, ok := extension.(json.RawMessage); ok {
 			err := yaml.Unmarshal(kuskExtension, &res)
 			if err != nil {
 				return nil, fmt.Errorf("failed to parse extension: %w", err)
+			}
+		}
+	}
+
+	for pathString, path := range spec.Paths {
+		var pathOpts options.Options
+
+		if extension, ok := path.Extensions[kuskExtensionKey]; ok {
+			if kuskExtension, ok := extension.(json.RawMessage); ok {
+				err := yaml.Unmarshal(kuskExtension, &pathOpts)
+				if err != nil {
+					return nil, fmt.Errorf("failed to parse extension: %w", err)
+				}
+
+				if res.PathOperations == nil {
+					res.PathOperations = map[string]options.Options{}
+				}
+
+				res.PathOperations[pathString] = pathOpts
+			}
+		}
+
+		for method, operation := range path.Operations() {
+			if extension, ok := operation.Extensions[kuskExtensionKey]; ok {
+				if kuskExtension, ok := extension.(json.RawMessage); ok {
+					var methodOpts options.Options
+					err := yaml.Unmarshal(kuskExtension, &methodOpts)
+					if err != nil {
+						return nil, fmt.Errorf("failed to parse extension: %w", err)
+					}
+
+					if res.PathOperations == nil {
+						res.PathOperations = map[string]options.Options{}
+					}
+
+					pathOpts = res.PathOperations[pathString]
+
+					if pathOpts.HTTPMethodOperations == nil {
+						pathOpts.HTTPMethodOperations = map[string]options.Options{}
+					}
+
+					pathOpts.HTTPMethodOperations[method] = methodOpts
+					res.PathOperations[pathString] = pathOpts
+				}
 			}
 		}
 	}
