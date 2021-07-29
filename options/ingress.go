62689d6b8a9de5952dbce38f4e296d47261f0cf4
Kyle Hodgetts <kyle@kubeshop.io>
2021-07-29 14:44:12 +0300

    Implement CORS options structure extension

--- options/ingress.go
+++ options/ingress.go
@@ -9,6 +9,17 @@ type IngressOptions struct {
 	// Host is an ingress host rule.
 	// See https://kubernetes.io/docs/concepts/services-networking/ingress/#ingress-rules for additional documentation.
 	Host string `yaml:"host,omitempty" json:"host,omitempty"`
+
+	CORS CORSOptions `yaml:"cors,omitempty" json:"cors,omitempty"`
+}
+
+type CORSOptions struct {
+	Origins        []string `yaml:"origins,omitempty" json:"origins,omitempty"`
+	Methods        []string `yaml:"methods,omitempty" json:"methods,omitempty"`
+	Headers        []string `yaml:"headers,omitempty" json:"headers,omitempty"`
+	ExposedHeaders []string `yaml:"exposed_headers,omitempty" json:"exposed_headers,omitempty"`
+	Credentials    bool     `yaml:"credentials,omitempty" json:"credentials,omitempty"`
+	MaxAge         int      `yaml:"max_age,omitempty" json:"max_age,omitempty"`
 }
 
 func (o *IngressOptions) Validate() error {
