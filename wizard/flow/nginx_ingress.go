b6083818ac7959d50ec6ee3da7a0bf70097b5e6a
Kyle Hodgetts <kyle@kubeshop.io>
2021-08-26 16:24:38 +0300

    nginx-ingress: Load defaults from apispec extensions

--- wizard/flow/nginx_ingress.go
+++ wizard/flow/nginx_ingress.go
@@ -14,24 +14,37 @@ type nginxIngressFlow struct {
 	baseFlow
 }
 
-func (n nginxIngressFlow) Start() (Response, error) {
+func (n nginxIngressFlow) getBasePath() string {
 	var basePathSuggestions []string
 	for _, server := range n.apiSpec.Servers {
 		basePathSuggestions = append(basePathSuggestions, server.URL)
 	}
 
-	basePath := n.prompt.SelectOneOf("Base path prefix", basePathSuggestions, true)
-	trimPrefix := n.prompt.Input("Prefix to trim from the URL (rewrite)", "")
-
-	separateMappings := false
-	if basePath != "" {
-		separateMappings = n.prompt.Confirm("Generate ingress resource for each endpoint separately?")
+	if len(basePathSuggestions) == 0 && n.opts.Path.Base != "" {
+		basePathSuggestions = append(basePathSuggestions, n.opts.Path.Base)
 	}
 
+	return n.prompt.SelectOneOf("Base path prefix", basePathSuggestions, true)
+}
+
+func (n nginxIngressFlow) getTrimPrefix(basePath string) string {
+	trimPrefixDefault := basePath
+	if n.opts.Path.TrimPrefix != "" {
+		trimPrefixDefault = n.opts.Path.TrimPrefix
+	}
+
+	return n.prompt.InputNonEmpty("Prefix to trim from the URL (rewrite)", trimPrefixDefault)
+}
+
+func (n nginxIngressFlow) shouldSeparateMappings(basePath string) bool {
+	return basePath != "" && n.prompt.Confirm("Generate ingress resource for each endpoint separately?")
+}
+
+func (n nginxIngressFlow) getTimeoutOpts() options.TimeoutOptions {
 	var timeoutOptions options.TimeoutOptions
 
 	// Support only request timeout as nginx-ingress generator doesn't support idle timeout
-	if requestTimeout := n.prompt.Input("Request timeout, leave empty to skip", ""); requestTimeout != "" {
+	if requestTimeout := n.prompt.Input("Request timeout, leave empty to skip", strconv.Itoa(int(n.opts.Timeouts.RequestTimeout))); requestTimeout != "" {
 		if rTimeout, err := strconv.Atoi(requestTimeout); err != nil {
 			log.Printf("WARN: %s is not a valid request timeout value. Skipping\n", requestTimeout)
 		} else {
@@ -39,33 +52,93 @@ func (n nginxIngressFlow) Start() (Response, error) {
 		}
 	}
 
+	return timeoutOptions
+}
+
+func (n nginxIngressFlow) getCORSOpts() options.CORSOptions {
 	var corsOpts options.CORSOptions
-	if setCORS := n.prompt.Confirm("Set CORS options?"); setCORS {
-		// Origins
-		corsOpts.Origins = []string{n.prompt.Input("add CORS origin", "")}
 
-		// Methods
-		corsOpts.Methods = n.prompt.InputMany("add CORS method")
-
-		// Headers
-		corsOpts.Headers = n.prompt.InputMany("add CORS header")
-
-		// ExposeHeaders
-		corsOpts.ExposeHeaders = n.prompt.InputMany("add CORS headers you want to expose")
-
-		// Credentials
-		credentials := n.prompt.Confirm("enable CORS credentials")
-		corsOpts.Credentials = &credentials
-
-		// Max age
-		maxAgeStr := n.prompt.Input("set CORS max age", "0")
-		maxAge, err := strconv.Atoi(maxAgeStr)
-		if err != nil {
-			log.Printf("WARN: %s is not a valid max age value. Skipping\n", maxAgeStr)
-			maxAge = 0
-		}
-		corsOpts.MaxAge = maxAge
+	// return if user doesn't want to set CORS options
+	if setCORS := n.prompt.Confirm("Set CORS options?"); !setCORS {
+		return corsOpts
 	}
+
+	// Origins
+	// If apispec has some origins set, prompt to use them, else prompt for input
+	if len(n.opts.CORS.Origins) > 0 && n.prompt.Confirm(fmt.Sprintf("add the following CORS origins? %s", n.opts.CORS.Origins)) {
+		corsOpts.Origins = n.opts.CORS.Origins
+	} else {
+		corsOpts.Origins = n.prompt.InputMany("add CORS origin")
+	}
+
+	// Methods
+	// If apispec has some methods set, prompt to use them, else prompt for input
+	if len(n.opts.CORS.Methods) > 0 && n.prompt.Confirm(fmt.Sprintf("add the following CORS methods? %s", n.opts.CORS.Methods)) {
+		corsOpts.Methods = n.opts.CORS.Methods
+	} else {
+		corsOpts.Methods = n.prompt.InputMany("add CORS method")
+	}
+
+	// Headers
+	// If apispec has some headers set, prompt to use them, else prompt for input
+	if len(n.opts.CORS.Headers) > 0 && n.prompt.Confirm(fmt.Sprintf("add the following CORS headers? %s", n.opts.CORS.Headers)) {
+		corsOpts.Headers = n.opts.CORS.Headers
+	} else {
+		corsOpts.Headers = n.prompt.InputMany("add CORS header")
+	}
+
+	// ExposeHeaders
+	// If apispec has some expose headers set, prompt to use them, else prompt for input
+	if len(n.opts.CORS.ExposeHeaders) > 0 && n.prompt.Confirm(fmt.Sprintf("add the following CORS expose headers? %s", n.opts.CORS.ExposeHeaders)) {
+		corsOpts.ExposeHeaders = n.opts.CORS.ExposeHeaders
+	} else {
+		corsOpts.ExposeHeaders = n.prompt.InputMany("add CORS headers you want to expose")
+	}
+
+	// Credentials
+	credentials := n.prompt.Confirm("enable CORS credentials")
+	corsOpts.Credentials = &credentials
+
+	// Max age
+	// default is what is set in apisec, or 0 if not set
+	maxAgeStr := n.prompt.Input("set CORS max age", strconv.Itoa(n.opts.CORS.MaxAge))
+	maxAge, err := strconv.Atoi(maxAgeStr)
+	if err != nil {
+		log.Printf("WARN: %s is not a valid max age value. Skipping\n", maxAgeStr)
+		maxAge = 0
+	}
+
+	corsOpts.MaxAge = maxAge
+
+	return corsOpts
+}
+
+func (n nginxIngressFlow) getCmdFromOpts(opts *options.Options) string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("kusk nginx-ingress -i %s ", n.apiSpecPath))
+	sb.WriteString(fmt.Sprintf("--namespace=%s ", n.targetNamespace))
+	sb.WriteString(fmt.Sprintf("--service.namespace=%s ", n.targetNamespace))
+	sb.WriteString(fmt.Sprintf("--service.name=%s ", n.targetService))
+	sb.WriteString(fmt.Sprintf("--path.base=%s ", opts.Path.Base))
+
+	if opts.Path.TrimPrefix != "" {
+		sb.WriteString(fmt.Sprintf("--path.trim_prefix=%s ", opts.Path.TrimPrefix))
+	}
+
+	if opts.Path.Split {
+		sb.WriteString("--path.split ")
+	}
+
+	return sb.String()
+}
+
+func (n nginxIngressFlow) Start() (Response, error) {
+	basePath := n.getBasePath()
+	trimPrefix := n.getTrimPrefix(basePath)
+	separateMappings := n.shouldSeparateMappings(basePath)
+
+	timeoutOptions := n.getTimeoutOpts()
+	corsOpts := n.getCORSOpts()
 
 	opts := &options.Options{
 		Namespace: n.targetNamespace,
@@ -82,20 +155,7 @@ func (n nginxIngressFlow) Start() (Response, error) {
 		CORS:     corsOpts,
 	}
 
-	var sb strings.Builder
-	sb.WriteString(fmt.Sprintf("kusk ambassador -i %s ", n.apiSpecPath))
-	sb.WriteString(fmt.Sprintf("--namespace=%s ", n.targetNamespace))
-	sb.WriteString(fmt.Sprintf("--service.namespace=%s ", n.targetNamespace))
-	sb.WriteString(fmt.Sprintf("--service.name=%s ", n.targetService))
-	sb.WriteString(fmt.Sprintf("--path.base=%s ", basePath))
-
-	if trimPrefix != "" {
-		sb.WriteString(fmt.Sprintf("--path.trim_prefix=%s ", trimPrefix))
-	}
-
-	if separateMappings {
-		sb.WriteString("--path.split ")
-	}
+	cmd := n.getCmdFromOpts(opts)
 
 	var ingressGenerator nginx_ingress.Generator
 	ingresses, err := ingressGenerator.Generate(opts, n.apiSpec)
@@ -104,7 +164,7 @@ func (n nginxIngressFlow) Start() (Response, error) {
 	}
 
 	return Response{
-		EquivalentCmd: sb.String(),
+		EquivalentCmd: cmd,
 		Manifests:     ingresses,
 	}, nil
 }
