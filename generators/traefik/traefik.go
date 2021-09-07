b54c9c239c8d654708815693526fb9fcabf1f76b
Kyle Hodgetts <kyle@kubeshop.io>
2021-09-07 12:43:56 +0300

    basic path prefix and trim implementation

--- generators/traefik/traefik.go
+++ generators/traefik/traefik.go
@@ -1,18 +1,27 @@
 package traefik
 
 import (
+	"bytes"
 	"fmt"
+	"text/template"
 
 	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/spf13/pflag"
+
 	"github.com/kubeshop/kusk/generators"
 	"github.com/kubeshop/kusk/options"
-	"github.com/spf13/pflag"
+)
+
+var (
+	ingressRouteTemplate *template.Template
 )
 
 const traefik = "traefik"
 
 func init() {
 	generators.Registry[traefik] = &Generator{}
+
+	ingressRouteTemplate = template.Must(template.New("ingressRoute").Parse(ingressRouteTpl))
 }
 
 type Generator struct{}
@@ -34,10 +43,34 @@ func (g *Generator) LongDescription() string {
 	return g.ShortDescription()
 }
 
-func (g *Generator) Generate(options *options.Options, spec *openapi3.T) (string, error) {
-	if err := options.FillDefaultsAndValidate(); err != nil {
-		return "", fmt.Errorf("failed to validate options: %w", err)
+func (g *Generator) Generate(opts *options.Options, spec *openapi3.T) (string, error) {
+	if err := opts.FillDefaultsAndValidate(); err != nil {
+		return "", fmt.Errorf("failed to validate opts: %w", err)
 	}
 
-	return "", nil
+	var data []ingressRouteData
+	op := ingressRouteData{
+		Name:             opts.Service.Name,
+		Namespace:        opts.Namespace,
+		Match:            generateMatchRule(opts),
+		ServiceName:      opts.Service.Name,
+		ServiceNamespace: opts.Service.Namespace,
+		ServicePort:      opts.Service.Port,
+	}
+
+	data = append(data, op)
+
+	var buf bytes.Buffer
+	err := ingressRouteTemplate.Execute(&buf, data)
+	if err != nil {
+		return "", fmt.Errorf("failed to execute ingress route tempalte: %w", err)
+	}
+
+	res := buf.String()
+
+	return res, nil
+}
+
+func generateMatchRule(opts *options.Options) string {
+	return fmt.Sprintf("\"PathPrefix(\\\"%s\\\")\"", opts.Path.Base)
 }
