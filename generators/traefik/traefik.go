bb341c41567317566abd25a6c764864b517b9268
Taras Yatsurak <tyatsurak@gmail.com>
2021-09-10 19:35:09 +0300

    Further work on traefik refactoring, adding basic CORS support

--- generators/traefik/traefik.go
+++ generators/traefik/traefik.go
@@ -2,6 +2,7 @@ package traefik
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/getkin/kin-openapi/openapi3"
@@ -18,9 +19,9 @@ import (
 
 const (
 	// This is default entrypoint, specified in static traefik configuration
-	HTTPEntryPoint  string = "web"
-	APIVersion      string = "traefik.containo.us/v1alpha1"
-	traefik         string = "traefik"
+	HTTPEntryPoint string = "web"
+	APIVersion     string = "traefik.containo.us/v1alpha1"
+	traefik        string = "traefik"
 )
 
 func init() {
@@ -64,55 +65,160 @@ func (g *Generator) LongDescription() string {
 	return g.ShortDescription()
 }
 
-type ingressRouteData struct {
-	Name             string
-	Namespace        string
-	Host             string
-	PathBase         string
-	PathTrimPrefix   string
-	ServiceName      string
-	ServiceNamespace string
-	ServicePort      int32
-}
-
 func (g *Generator) Generate(opts *options.Options, spec *openapi3.T) (string, error) {
 	if err := opts.FillDefaultsAndValidate(); err != nil {
 		return "", fmt.Errorf("failed to validate opts: %w", err)
 	}
-	ingressData := ingressRouteData{
-		Name:             opts.Service.Name,
-		Namespace:        opts.Namespace,
-		Host:             opts.Host,
-		PathBase:         opts.Path.Base,
-		PathTrimPrefix:   opts.Path.TrimPrefix,
-		ServiceName:      opts.Service.Name,
-		ServiceNamespace: opts.Service.Namespace,
-		ServicePort:      opts.Service.Port,
+	// TODO: do we really need this for Traefik?? Single Ingress looks ok.
+	if opts.Path.Split {
+		return generateSplitIngress(opts, spec)
 	}
+	return generateCombinedIngress(opts, spec)
+}
 
+func generateCombinedIngress(opts *options.Options, spec *openapi3.T) (string, error) {
 	service := traefikCRD.Service{
 		LoadBalancerSpec: traefikCRD.LoadBalancerSpec{
-			Name:      ingressData.ServiceName,
-			Namespace: ingressData.ServiceNamespace,
-			Port:      intstr.IntOrString{IntVal: ingressData.ServicePort},
+			Name:      opts.Service.Name,
+			Namespace: opts.Service.Namespace,
+			Port:      intstr.IntOrString{IntVal: opts.Service.Port},
 		},
 	}
-	middlewares := generateMiddlewares(ingressData)
-	route := traefikCRD.Route{
-		Match:    generateMatchRule(ingressData),
-		Services: []traefikCRD.Service{service},
-		Kind:     "Rule", Middlewares: generateMiddlewaresRefs(middlewares),
+	host := opts.Host
+	base := opts.Path.Base
+	// K8s name for created resources are based on service name
+	name := opts.Service.Name
+	// these middlewares will be used for all paths
+	baseMiddlewares := generateBaseMiddlewares(name, opts)
+	// these are all middlewares to create manifests
+	// any new created middleware must go here
+	allMiddlewares := append([]traefikCRD.Middleware{}, baseMiddlewares...)
+	routes := []traefikCRD.Route{}
+	// Iterate on all paths and build routes rules
+	for path, pathItem := range spec.Paths {
+		// x-kusk options per path
+		pathSubOptions, ok := opts.PathSubOptions[path]
+		if ok {
+			if pathSubOptions.Disabled {
+				continue
+			}
+		}
+
+		if pathSubOptions.Host != "" && pathSubOptions.Host != host {
+			host = pathSubOptions.Host
+		}
+		// take global CORS options
+		corsOpts := opts.CORS
+
+		// if non-zero path-level CORS options are different, override with them
+		if pathSubOpts, ok := opts.PathSubOptions[path]; ok {
+			if !reflect.DeepEqual(options.CORSOptions{}, pathSubOpts.CORS) &&
+				!reflect.DeepEqual(corsOpts, pathSubOpts.CORS) {
+				corsOpts = pathSubOpts.CORS
+			}
+		}
+		pathMiddlewares := []traefikCRD.Middleware{}
+		// if final CORS options are not empty, include them
+		if !reflect.DeepEqual(options.CORSOptions{}, corsOpts) {
+			corsMiddleware := generateCORSMiddleware(name, path, opts, corsOpts)
+			pathMiddlewares = append(pathMiddlewares, corsMiddleware)
+			// Add to manifests generation
+			allMiddlewares = append(allMiddlewares, pathMiddlewares...)
+		}
+		// x-kusk options per operation (http method)
+		// For each method we create separate Match rule and route in case there are x-kusk extention overrides
+		for method := range pathItem.Operations() {
+			opSubOptions, ok := opts.OperationSubOptions[method+path]
+			if ok {
+				if opSubOptions.Disabled {
+					continue
+				}
+				if opSubOptions.Host != "" && opSubOptions.Host != host {
+					host = opSubOptions.Host
+				}
+			}
+
+			matchRule := generateMatchRule(host, base, path, method)
+			route := traefikCRD.Route{
+				Match:    matchRule,
+				Services: []traefikCRD.Service{service},
+				Kind:     "Rule", Middlewares: generateMiddlewaresRefs(append(baseMiddlewares, pathMiddlewares...)),
+			}
+			routes = append(routes, route)
+		}
 	}
 	ingressRouteSpec := traefikCRD.IngressRouteSpec{
 		EntryPoints: []string{HTTPEntryPoint},
-		Routes:      []traefikCRD.Route{route},
+		Routes:      routes,
 	}
+	// This is finished Ingress object
 	ingressRoute := traefikCRD.IngressRoute{
 		Spec:       ingressRouteSpec,
 		TypeMeta:   metav1.TypeMeta{Kind: "IngressRoute", APIVersion: APIVersion},
-		ObjectMeta: metav1.ObjectMeta{Name: ingressData.Name, Namespace: ingressData.Namespace},
+		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: opts.Service.Namespace},
 	}
-	return buildOutput(ingressRoute, middlewares)
+	return buildOutput(ingressRoute, allMiddlewares)
+}
+
+// TODO: write split ingress
+func generateSplitIngress(opts *options.Options, spec *openapi3.T) (string, error) {
+	return "", nil
+}
+
+// Separate CORS middleware
+func generateCORSMiddleware(name string, path string, opts *options.Options, corsOpts options.CORSOptions) traefikCRD.Middleware {
+	midlewareSpec := traefikCRD.MiddlewareSpec{Headers: &traefikDynamicConfig.Headers{
+		AccessControlAllowHeaders:     corsOpts.Headers,
+		AccessControlAllowMethods:     corsOpts.Methods,
+		AccessControlAllowOriginList:  corsOpts.Origins,
+		AccessControlMaxAge:           int64(corsOpts.MaxAge),
+		AccessControlAllowCredentials: *corsOpts.Credentials,
+	}}
+	middleware := traefikCRD.Middleware{
+		TypeMeta:   metav1.TypeMeta{Kind: "Middleware", APIVersion: APIVersion},
+		ObjectMeta: metav1.ObjectMeta{Name: fmt.Sprintf("%s-%s-cors-headers", name, path), Namespace: opts.Namespace},
+		Spec:       midlewareSpec,
+	}
+	return middleware
+}
+
+// Any non-overridable middlewares that apply to all paths
+func generateBaseMiddlewares(name string, opts *options.Options) []traefikCRD.Middleware {
+	var middlewares []traefikCRD.Middleware
+	if opts.Path.TrimPrefix != "" {
+		midlewareSpec := traefikCRD.MiddlewareSpec{StripPrefix: &traefikDynamicConfig.StripPrefix{Prefixes: []string{opts.Path.TrimPrefix}}}
+		middleware := traefikCRD.Middleware{
+			TypeMeta:   metav1.TypeMeta{Kind: "Middleware", APIVersion: APIVersion},
+			ObjectMeta: metav1.ObjectMeta{Name: fmt.Sprintf("%s-strip-base-prefix", name), Namespace: opts.Namespace},
+			Spec:       midlewareSpec,
+		}
+		middlewares = append(middlewares, middleware)
+	}
+	return middlewares
+}
+
+func generateMiddlewaresRefs(middlewares []traefikCRD.Middleware) []traefikCRD.MiddlewareRef {
+	var middlewaresRefs []traefikCRD.MiddlewareRef
+	for _, m := range middlewares {
+		middlewaresRefs = append(middlewaresRefs, traefikCRD.MiddlewareRef{Name: m.Name, Namespace: m.Namespace})
+	}
+	return middlewaresRefs
+}
+
+func generateMatchRule(host string, base string, path string, method string) string {
+	const httpPathSeparator string = "/"
+	// Avoids path joins (removes // in e.g. /path//subpath, or //subpath)
+	fullPath := fmt.Sprintf(`%s/%s`, strings.TrimSuffix(base, httpPathSeparator), strings.TrimPrefix(path, httpPathSeparator))
+	// Create rules to filter request on
+	rules := []string{}
+	// Host filter
+	if host != "" {
+		rules = append(rules, fmt.Sprintf("Host(`%s`)", host))
+	}
+	rules = append(rules, fmt.Sprintf("PathPrefix(`%s`)", fullPath))
+	rules = append(rules, fmt.Sprintf("Method(`%s`)", method))
+	// returns e.g. Host(`example.org`) && PathPrefix(`/petstore/api/v3/pet`) && Method(`POST`)
+	return strings.Join(rules, " && ")
 }
 
 // Build suitable output to be piped into kubectl or a file
@@ -136,34 +242,4 @@ func buildOutput(ingressRoute traefikCRD.IngressRoute, middlewares []traefikCRD.
 	}
 	builder.WriteString(string(b))
 	return builder.String(), nil
-}
-
-func generateMiddlewares(ingressData ingressRouteData) []traefikCRD.Middleware {
-	var middlewares []traefikCRD.Middleware
-	if ingressData.PathTrimPrefix != "" {
-		midlewareSpec := traefikCRD.MiddlewareSpec{StripPrefix: &traefikDynamicConfig.StripPrefix{Prefixes: []string{ingressData.PathTrimPrefix}}}
-		middleware := traefikCRD.Middleware{
-			TypeMeta:   metav1.TypeMeta{Kind: "Middleware", APIVersion: APIVersion},
-			ObjectMeta: metav1.ObjectMeta{Name: fmt.Sprintf("%s-strip-prefixes", ingressData.Name), Namespace: ingressData.Namespace},
-			Spec:       midlewareSpec,
-		}
-		middlewares = append(middlewares, middleware)
-	}
-	return middlewares
-}
-
-func generateMiddlewaresRefs(middlewares []traefikCRD.Middleware) []traefikCRD.MiddlewareRef {
-	var middlewaresRefs []traefikCRD.MiddlewareRef
-	for _, m := range middlewares {
-		middlewaresRefs = append(middlewaresRefs, traefikCRD.MiddlewareRef{Name: m.Name, Namespace: m.Namespace})
-	}
-	return middlewaresRefs
-}
-
-func generateMatchRule(ingressData ingressRouteData) string {
-	if ingressData.Host == "" {
-		return fmt.Sprintf("PathPrefix(\"%s\")", ingressData.PathBase)
-	} else {
-		return fmt.Sprintf("Host(\"%s\") && PathPrefix(\"%s\")", ingressData.Host, ingressData.PathBase)
-	}
 }
